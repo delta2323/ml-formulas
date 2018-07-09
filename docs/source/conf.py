2891ee604ed2dfaf672c8b7f494b1c3832709d8b
Kenta OONO <oono@preferred.jp>
2018-07-09 15:46:47 +0900

    Fix extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
 
 from recommonmark.parser import CommonMarkParser
 
@@ -36,7 +33,10 @@ source_parsers = {
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax']
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+extensions = ['mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
