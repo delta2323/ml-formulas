4dd53732b020e5cedfccb03a8ecca83a6bf95f93
Kenta OONO <oono@preferred.jp>
2018-07-09 15:49:13 +0900

    Revert "Fix extension"
    This reverts commit 2891ee604ed2dfaf672c8b7f494b1c3832709d8b.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,6 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 from recommonmark.parser import CommonMarkParser
 
@@ -33,10 +36,7 @@ source_parsers = {
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-extensions = ['mathjax']
+extensions = ['sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
