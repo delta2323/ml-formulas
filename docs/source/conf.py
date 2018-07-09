5e859e2c743d573e9fd49687945bcf307d172d9d
Kenta OONO <oono@preferred.jp>
2018-07-09 15:07:11 +0900

    Support tex macros

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 from recommonmark.parser import CommonMarkParser
 
@@ -36,7 +36,7 @@ source_parsers = {
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax']
+extensions = ['mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -174,5 +174,5 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
-
+import tex_macro
+mathjax_preamble = tex_macro.macro
