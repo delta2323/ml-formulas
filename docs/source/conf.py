fcf48e591067bbb0944c754fc0bde9330a8e100b
Kenta OONO <oono@preferred.jp>
2018-07-20 23:28:16 +0900

    Fix mathjax_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ source_parsers = {
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['mathjax']
-mathjax_path = 'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
+mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.3/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
